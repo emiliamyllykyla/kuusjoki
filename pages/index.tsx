a6f3dffc5ca55bb72761b6c8f4aff270adc83d47
Emilia <emilia.myllykyla@hotmail.com>
2021-08-27 19:50:39 +0300

    Add the best footer in the universe, and make the hero win nobel price

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,41 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Image from "next/image";
+import cow from "../public/cow.jpeg";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      <div className={styles.hero}>
+        <Image
+          className={styles.heroImage}
+          src={cow}
+          objectFit="cover"
+          objectPosition="center"
+          alt="Cow"
+          layout="fill"
+        />
+      </div>
+      <div className={styles.main}>
+        <h2>Tervetuloa Kuusjoki-Seura ry:n sivuille!</h2>
+        <p>
+          Tutustu Arkiston ja Viljamakasiinin toimintaan. Tul mukka! Meil o
+          mukava!
+        </p>
+        <h3>Tule kurkkaamaan arkistolle!</h3>
+        <p>Etsitkö jotain tietoa tai kuvaa Kuusjokeen liittyen?</p>
+        <p>
+          Haluaisitko tulla Arkistolle tutkimaan vanhoja kuvia, lehtiä, karttoja
+          tai muuta materiaalia Kuusjoelta? Autamme parhaamme mukaan.
+        </p>
+        <p>
+          Voit ottaa yhteyttä sähköpostitse. Osoitteet löytyvät kohdasta
+          yhteystiedot.
+        </p>
+        Tervetuloa!
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
